cbd0048b52a05d9e43676b693b01ee79e14fdf9a
sergigp <sergi.gonzalez@akamon.com>
2014-08-09 18:24:27 +0200

    exercism 1

--- bob/bob.clj
+++ bob/bob.clj
@@ -1,19 +1,19 @@
 (ns bob (:use [clojure.string]))
 
-(defn is-question? [message]
+(defn question? [message]
   (= (last message) \?))
 
-(defn contains-some-letter [message]
+(defn contains-some-letter? [message]
   (not (empty? (re-seq #"[a-zA-Z]" message))))
 
-(defn is-yelling? [message]
-  (and (contains-some-letter message) (= (upper-case message) message)))
+(defn yelling? [message]
+  (and (contains-some-letter? message) (= (upper-case message) message)))
 
 (defn response-for [message]
   (cond
    (blank? message) "Fine. Be that way!"
-   (is-yelling? message) "Woah, chill out!"
-   (is-question? message) "Sure."
+   (yelling? message) "Woah, chill out!"
+   (question? message) "Sure."
    :else "Whatever."))
 
 
